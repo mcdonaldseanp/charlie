85eeb02aff457e198c3ebc3f7124c8aa3e57bc4d
Sean P. McDonald <sean.mcdonald@puppet.com>
2022-06-08 16:04:21 -0500

    (release) Update to new version v0.0.7

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ import (
 	"github.com/mcdonaldseanp/charlie/validator"
 )
 
-const VERSION string = "v0.0.6"
+const VERSION string = "v0.0.7"
 
 func UpdateVersion(version_file string, new_version string) *airer.Airer {
 	arr := validator.ValidateParams(fmt.Sprintf(
