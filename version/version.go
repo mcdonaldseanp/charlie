d738f3a8c4f8b17caa1bce51a8d734d369ffc7dd
Sean P. McDonald <mcd.seanp@gmail.com>
2026-04-10 12:58:50 -0500

    (release) Update to new version v0.1.1

--- version/version.go
+++ version/version.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-const VERSION string = "v0.1.0"
+const VERSION string = "v0.1.1"
 
 func ReleaseArtifact(name string) string {
 	return fmt.Sprintf("https://github.com/mcdonaldseanp/charlie/releases/download/%s/%s", VERSION, name)
